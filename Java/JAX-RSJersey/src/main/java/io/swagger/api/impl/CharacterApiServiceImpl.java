f29e601d6804a1a742100ecd98c37bc2a5557784
salawum <37109574+salawum@users.noreply.github.com>
2021-05-06 00:23:37 +0100

    Finished the Character database for Java

--- Java/JAX-RSJersey/src/main/java/io/swagger/api/impl/CharacterApiServiceImpl.java
+++ Java/JAX-RSJersey/src/main/java/io/swagger/api/impl/CharacterApiServiceImpl.java
@@ -10,6 +10,7 @@ import io.swagger.model.*;
 import io.swagger.model.Character;
 import io.swagger.model.CharacterList;
 
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.List;
 import io.swagger.api.NotFoundException;
@@ -45,20 +46,6 @@ import javax.validation.constraints.*;
             cStmt.setString(10, body.getSpecies());
 
             rs = cStmt.executeQuery();
-            while (rs.next()) {
-                System.out.println(
-                    rs.getString("name")+", "+
-                    rs.getString("height")+", "+
-                    rs.getString("mass")+", "+
-                    rs.getString("hair_color")+", "+
-                    rs.getString("skin_color")+", "+
-                    rs.getString("eye_color")+", "+
-                    rs.getString("birth_year")+", "+
-                    rs.getString("gender")+", "+
-                    rs.getString("homeworld")+", "+
-                    rs.getString("species")
-                );
-            }
         } catch (SQLException ex) {
             return HelperFunctions.HandleError(ex);
         }
@@ -67,7 +54,6 @@ import javax.validation.constraints.*;
     @Override
     public Response deleteCharacter(String name, SecurityContext securityContext) throws NotFoundException {
         System.out.println("deleteCharacter");
-        System.out.println(HelperFunctions.getConnectionString());
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
@@ -88,17 +74,104 @@ import javax.validation.constraints.*;
     }
     @Override
     public Response getCharacter(String name, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        System.out.println("getCharacter");
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DriverManager.getConnection(HelperFunctions.getConnectionString());
+            CallableStatement cStmt = conn.prepareCall("{CALL getCharacter(?)}");
+            cStmt.setString(1, name);
+            rs = cStmt.executeQuery();
+
+            ArrayList<Character> c_list = new ArrayList<>();
+            while (rs.next()) {
+                Character c = new Character();
+                c.setName(rs.getString("name"));
+                c.setHeight(rs.getInt("height"));
+                c.setMass(rs.getBigDecimal("mass"));
+                c.setHairColor(rs.getString("hair_color"));
+                c.setSkinColor(rs.getString("skin_color"));
+                c.setEyeColor(rs.getString("eye_color"));
+                c.setBirthYear(rs.getString("birth_year"));
+                c.setGender(rs.getString("gender"));
+                c.setHomeworld(rs.getString("homeworld"));
+                c.setSpecies(rs.getString("species"));
+                c_list.add(c);
+                System.out.println(
+                    c.getName()+", "+
+                    c.getHeight()+", "+
+                    c.getMass()+", "+
+                    c.getHairColor()+", "+
+                    c.getSkinColor()+", "+
+                    c.getEyeColor()+", "+
+                    c.getBirthYear()+", "+
+                    c.getGender()+", "+
+                    c.getHomeworld()+", "+
+                    c.getSpecies()
+                );
+            }
+            if(c_list.size() < 1){
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+            return Response.ok().entity(c_list.get(0)).build();
+        } catch (SQLException ex) {
+            return HelperFunctions.HandleError(ex);
+        }
     }
     @Override
     public Response getMass(Integer mass, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        System.out.println("getMass");
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DriverManager.getConnection(HelperFunctions.getConnectionString());
+            CallableStatement cStmt = conn.prepareCall("{CALL getMass(?)}");
+            cStmt.setInt(1, mass);
+            rs = cStmt.executeQuery();
+
+            ArrayList<String> name_list = new ArrayList<>();
+            while (rs.next()) {
+                name_list.add(rs.getString("name"));
+                System.out.println(rs.getString("name"));
+            }
+            if(name_list.size() < 1){
+                return Response.status(Response.Status.NOT_FOUND).build();
+            }
+            return Response.ok().entity(name_list).build();
+        } catch (SQLException ex) {
+            return HelperFunctions.HandleError(ex);
+        }
     }
     @Override
     public Response updateCharacter(Character body, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        System.out.println("updateCharacter");
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn = DriverManager.getConnection(HelperFunctions.getConnectionString());
+
+            CallableStatement cStmt = conn.prepareCall("{CALL updateCharacter(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)}");
+            cStmt.setString(1, body.getName());
+            cStmt.setInt(2, body.getHeight());
+            cStmt.setBigDecimal(3, body.getMass());
+            cStmt.setString(4, body.getHairColor());
+            cStmt.setString(5, body.getSkinColor());
+            cStmt.setString(6, body.getEyeColor());
+            cStmt.setString(7, body.getBirthYear());
+            cStmt.setString(8, body.getGender());
+            cStmt.setString(9, body.getHomeworld());
+            cStmt.setString(10, body.getSpecies());
+
+            int updated = cStmt.executeUpdate();
+            if(updated >= 1){
+                return Response.status(Response.Status.OK).build();
+            }
+            return Response.status(Response.Status.NOT_FOUND).build();
+        } catch (SQLException ex) {
+            return HelperFunctions.HandleError(ex);
+        }
     }
 }
